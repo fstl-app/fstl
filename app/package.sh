70f0297d209e04f3de354a3684759eecc6077719
Matt Keeter <matt.j.keeter@gmail.com>
2014-03-24 20:50:06 -0400

    Small bash script to run macdeployqt then clean up unused frameworks

---
+++ app/package.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd ../build
+macdeployqt fstl.app
+cd fstl.app/Contents/PlugIns
+rm -rf accessible audio imageformats mediaservice playlistformats position printsupport qml1tooling sensorgestures sensors
+cd ../Frameworks
+rm -rf QtDeclarative.framework QtMultimedia.framework QtMultimediaWidgets.framework QtNetwork.framework QtPositioning.framework QtQml.framework QtQuick.framework QtScript.framework QtSensors.framework QtSql.framework QtXmlPatterns.framework
+cd ../Resources
+rm empty.lproj
+
