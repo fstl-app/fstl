f66d49388c71e0d17050f13ed9f90357e5818ff1
Matt Keeter <matt.j.keeter@gmail.com>
2017-04-28 09:12:15 -0400

    Clean up win32 packaging script

--- exe/package.sh
+++ exe/package.sh
@@ -1,3 +1,13 @@
-cd ..
-cp build/release/fstl.exe .
-/c/Program\ Files/7-Zip/7z.exe a fstl_win.zip fstl.exe README.md
+#!/bin/sh
+set -e -x
+
+cd ../build
+make clean
+rm -rf fstl.exe
+make -j8
+
+cp ../README.md README.txt
+cp release/fstl.exe .
+/c/Program\ Files/7-Zip/7z.exe a fstl.zip fstl.exe README.txt
+cp fstl.zip ..
+rm README.txt fstl.exe
