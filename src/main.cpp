8afbaee63a37124d6dd8e109b278d598c63ecd59
William Daniau <william.daniau@femto-st.fr>
2023-01-30 13:22:09 +0100

    Force C locale to force decimal point use in entries.

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,14 @@
 
 int main(int argc, char *argv[])
 {
+    // Force C locale to force decimal point
+    QLocale::setDefault(QLocale::c());
+
     QCoreApplication::setOrganizationName("fstl-app");
     QCoreApplication::setOrganizationDomain("https://github.com/fstl-app/fstl");
     QCoreApplication::setApplicationName("fstl");
     QCoreApplication::setApplicationVersion(FSTL_VERSION);
     App a(argc, argv);
+
     return a.exec();
 }
