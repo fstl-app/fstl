9ba9c8af1c26325f427c1c2c6d9f6ba43fcb0dd8
Teemu Ikonen <tpikonen@mailbox.org>
2024-04-25 12:25:28 +0300

    Do not reset rotation on 'Center the model'

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -88,19 +88,19 @@ void Canvas::view_anim(float v)
 
 void Canvas::common_view_change(enum ViewPoint c)
 {
+    if (c == centerview) {
+        scale = default_scale;
+        center = default_center;
+        zoom = 1;
+        update();
+        return;
+    }
+
     currentTransform.setToIdentity();
     currentTransform.rotate(180.0, QVector3D(0, 0, 1));
 
     switch (c)
     {
-    case centerview:
-        {
-            scale = default_scale;
-            center = default_center;
-            zoom = 1;
-        }
-        break;
-
     case topview:
         {
             currentTransform.rotate(180, QVector3D(1, 0, 0));
